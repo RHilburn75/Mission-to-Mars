643af022c12cdffe813b9493fc048f42d50b969f
RHilburn75 <94253815+RHilburn75@users.noreply.github.com>
2022-02-06 13:20:22 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
-#Set up Flask
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -18,8 +17,8 @@ def index():
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
+   mars.update({}, mars_data, upsert=True)
+   return "Scraping Successful!"
 
 if __name__ == "__main__":
    app.run()
